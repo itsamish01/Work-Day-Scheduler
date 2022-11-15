050e1d4a23b92ad515bdd537e81a4f8b5d0eb9b4
AyeMishDior <itsamish02@gmail.com>
2022-11-14 19:59:48 -0600

    final commit changes to css html and js

--- Develop/script.js
+++ Develop/script.js
@@ -2,33 +2,28 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var saveBtn = $(".saveBtn");
+// current status of calendar
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
 
 //this function will show the current day on the top of the page
 
-$("#currentDay").text(moment().format('dddd MMMM do YYYY'));
+// this will show the hour of each day
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
 
-// so this will be the current day displayed on the top of the calendar
- function timeBlockColor() {
-  var hour = moment().hours();
-
-  $(".time-block").each(function() {
-    var currHour = parseInt($(this).attr("id"));
-
-    //will be using a if-then statement to indicate whether it will be past, present or future.
-
-    if (currHour > hour) {
-      $(this).addClass("future");
-    } else if (currHour === hour) {
-      $(this).addClass("present");
-    } else {
-        $(this).addClass("past");
-    }
-  })
-
- };
-
-
+var hour = moment().hours();
+var userInput;
+var hourSpan;
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -37,48 +32,86 @@ $("#currentDay").text(moment().format('dddd MMMM do YYYY'));
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  // WHEN I click the save button for that time block
-  saveBtn.on("click", function() {
+  var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
 
-    var time = $(this).siblings(".hour").text();
-    var plan = $(this).siblings(".plan").val();
+  function initPage() {
 
-    //THEN this function will save in the local storage
-
-    localStorage.setItem(time, plan);
-  });
-
-  // When I refresh the page it will save the information
-  function usePlanner() {
-    $(".hour").each(function() {
-      var currHour = $(this).text();
-      var currPlan = localStorage.getItem(currHour);
-
-      //this will be the console.log for current hour
-
-
-      if(currPlan !== null) {
-        $(this).siblings(".plan").val(currPlan);
-      }
-   });
-  }
-
+    console.log("Current Hour " + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
   
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
-  // call functions
-
-  timeBlockColor();
-  usePlanner()
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+    tenAm.val(init10);
+    
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+    elevenAm.val(init11);
+    
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+    twelvePm.val(init12);
+    
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+    onePm.val(init1);
+    
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+    twoPm.val(init2);
+    
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+    threePm.val(init3);
+   
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+    fourPm.val(init4);
+    
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+    fivePm.val(init5);
+    
+    var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+    sixPm.val(init6);
+    
+    var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+    sevenPm.val(init7);
+  } 
+  
+  function background () {
+        
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+  
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+  }
+  
+  $(document).ready(function(){
+    initPage()
+    background()
+  
+    // Buttons that will save in local storage
+    $(".saveBtn").on("click", function(){
+      userInput = $(this).siblings(".form-control").val().trim();
+      console.log(userInput);
+      hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+      console.log(hourSpan);
+      localStorage.setItem(hourSpan, JSON.stringify(userInput));
+  
+    })
+    // clear message for the day
+    $("#clearDay").on("click", function(){
+      localStorage.clear();
+      initPage()
+    }) 
+  
+  });
